d47f81fe541d27e8042c5e315e73c6db86a1b4df
brandonday7 <brandontday7@gmail.com>
2017-09-27 20:28:37 +0000

    createMenuItem function created and tested with local data

---
+++ public/scripts/testCreate.js
@@ -0,0 +1,10 @@
+let x = require('./createMenuElement.js')
+
+let item = {
+  name: 'dfjvndf',
+  price: 'dfvjnfgb',
+  description: 'dfgunfv',
+  image: 'dfhnf'
+};
+
+console.log(x.createMenuElement(item));
