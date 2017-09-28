2aee16d911ae900f89a1f30dc1bb949cf19df0ad
brandonday7 <brandontday7@gmail.com>
2017-09-28 22:27:21 +0000

    added and test a subtract button to the main page. minor refactoring

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -16,23 +16,50 @@ $(document).ready(function() {
       name: here.find('p.item-title').text(),
       price: here.find('p.item-price').text().replace(/\$/, ''),
     }
-    if (order.items.filter(function (ordered) {
+
+    let current = order.items.filter(function (ordered) {
       return ordered.id === item.id
-    }).length !== 0) {
-      order.items.filter(function (ordered) {
-        return ordered.id === item.id
-      })[0].quantity++;
+    })
+    if (current.length !== 0) {
+      current[0].quantity++;
       order.total += (item.price *1);
     } else {
       item.quantity = 1;
       order.items.push(item);
       order.total += (item.price *1);
     }
-    $('#order-total').text(`$${order.total} + tax`)
-    console.log('Current order: ',order);
+
+    updateTotals(order);
+  })
 
 
-    reviewOrderPane(order);
+  $('.menu-item > .remove-from-order').on('click', function (evt) {
+    evt.preventDefault();
+    var here = $(this).closest('.menu-item');
+    var item = {
+      id: here.find('input').data('item'),
+      name: here.find('p.item-title').text(),
+      price: here.find('p.item-price').text().replace(/\$/, ''),
+    }
+
+    let current = order.items.filter(function (ordered) {
+      return ordered.id === item.id
+    })
+    if (current.length !== 0) {
+      current[0].quantity--;
+      order.total -= (item.price *1);
+      //at this point, if quantity is 0, remove item from array
+      if (current[0].quantity === 0) {
+        for (index in order.items) {
+          if (order.items[index].quantity === 0) {
+            order.items.splice(index, 1);
+          }
+        }
+      }
+
+      }
+      updateTotals(order);
+
   })
 
   $('#order-submit').on('click', function(evt) {
@@ -81,7 +108,12 @@ function createItemInPane(item) {
   return formattedItem;
 }
 
+function updateTotals(order) {
+  $('#order-total').text(`$${order.total} + tax`)
+    console.log('Current order: ',order);
 
+    reviewOrderPane(order);
+}
 
 
 
