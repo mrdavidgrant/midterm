2e0cb675c6a6cec58cd2eabe9afbc54f5237cc46
MilesWilde <craigmilde_@hotmail.com>
2017-09-28 15:01:03 +0000

    calls&sms alpha stages

--- twilioServerStuff.js
+++ twilioServerStuff.js
@@ -1,15 +1,28 @@
-const twilio      = require('twilio');
-const client      = new twilio(accountSid, authToken);
-
 const accountSid = 'ACef0ec881dd1e44aebea6f229a45bc45b'; // Your Account SID from www.twilio.com/console
 const authToken = 'b6fb5011aa8a061f90e157c8d6c32467';   // Your Auth Token from www.twilio.com/console
 
-app.post("/", (req, res) => {
+const twilio      = require('twilio');
+const client      = new twilio(accountSid, authToken);
+
+function messageSMS(){ 
   client.messages.create({
-      body: 'Hello from Node',
+      body: '',
       to: '+16474568825',  // Text this number
       from: '+14373715931' // From a valid Twilio number
   })
   .then((message) => console.log(message.sid));  
-  res.render("index");
-});
+}
+
+function messageCall(){
+  client.calls.create({
+      url: "http://demo.twilio.com/docs/voice.xml",
+      to: "+16474568825",
+      from: "+14373715931"
+  }, function(err, call) {
+      process.stdout.write(call.sid);
+  });
+}
+module.exports = {
+  messageSMS: messageSMS,
+  messageCall: messageCall
+}
