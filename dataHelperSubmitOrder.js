7c0d95a900f0b10e32237e68670c013147c504c8
MilesWilde <craigmilde_@hotmail.com>
2017-09-29 12:54:24 +0000

    changes to algorithm for submitordersdbhelp

--- dataHelperSubmitOrder.js
+++ dataHelperSubmitOrder.js
@@ -2,10 +2,8 @@ require('dotenv').config();
 module.exports = function(knex){
   return {
     orderDB: function(ready, personOrder){
-      console.log('orderDB called')
-
-      knex('users').max('id').then((userMax) =>{
-        console.log(Number(userMax[0].max) + 1);
+      console.log('orderDB called, personOrder: ', personOrder)
+       
         knex('users').insert({
           email: personOrder.email,
           first_name: personOrder.firstName,
@@ -15,25 +13,21 @@ module.exports = function(knex){
           .then(res => {
 
             knex('orders').insert({
-              user_id: (Number(userMax[0].max) + 1),
+              user_id: res.rowCount,
               time_ready: ready
             })
             .then(order => {
-              knex('orders').max('id').then(result => {
-                
-                for (let i in personOrder.items) {
-                  console.log('result', result)
-                  knex('order_items').insert({ 
-                    order_id: (Number(result[0].max) + 1),
-                    item_id: personOrder.items[i].id,
-                    quantity: personOrder.items[i].quantity
-                  })
-                } 
-              })
+              console.log("in order_items")
+              for (let i in personOrder.items) {
+                knex('order_items').insert({ 
+                  order_id: res.rowCount,
+                  item_id: personOrder.items[i].id,
+                  quantity: personOrder.items[i].quantity
+                }).then()
+              } 
             }) 
           }
         )
-      })   
+      }   
     }
   }
-}
