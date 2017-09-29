bdc555561b3cb5359ad37f8681ff0bf31bd14ead
MilesWilde <craigmilde_@hotmail.com>
2017-09-29 18:50:24 +0000

    twilio stuff

--- twilioServerStuff.js
+++ twilioServerStuff.js
@@ -21,11 +21,10 @@ function messageCall(person){
   const accountSid = 'ACef0ec881dd1e44aebea6f229a45bc45b'; // Your Account SID from www.twilio.com/console
   const authToken = 'b6fb5011aa8a061f90e157c8d6c32467';   // Your Auth Token from www.twilio.com/console
 
-
   const twilio      = require('twilio');
   const client      = new twilio(accountSid, authToken);
   client.calls.create({
-      url: "./voice.xml",
+      url: "http://demo.twilio.com/docs/voice.xml",
       to: "+16474568825",
       from: "+14373715931"
   }, function(err, call) {
