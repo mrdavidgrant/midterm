482a18fbf32053c7c85629cb30e4ddeb55df47b2
David Grant <mrdavidgrant@outlook.com>
2017-10-01 16:48:08 -0400

    changing state to production for heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 const PORT        = process.env.PORT || 8000;
-const ENV         = process.env.ENV || "development";
+const ENV         = process.env.ENV || "production";
 const express     = require("express");
 const bodyParser  = require("body-parser");
 const sass        = require("node-sass-middleware");
